0f66dde815042837bedd1ad59ff60901173edbaf
Frontbeard <lufbenitez@gmail.com>
2024-11-20 03:05:39 -0300

    falta ultimar detalles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="px-2 py-2">
+        <div className="flex items-center justify-center text-center px-7 py-7 bg-gray-100 rounded-lg">
+          <p className="w-full">
+          © 2024 Beardfolio by <a href="https://github.com/Frontbeard" className="font-medium underline" >Frontbeard</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
